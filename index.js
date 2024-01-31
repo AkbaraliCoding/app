86bca0b52667649e0318a32038bba20535984753
akbaralicoding <nematjonovakbarali19@gmail.com>
2024-01-31 20:19:20 +0500

    Handlebars

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import {engine ,create} from 'express-handlebars'
+
+const app = express()
+
+const hbs = create({
+    defaultLayout: "main",
+    extname:"hbs"
+})
+
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+
+
+
+app.get('/', (req, res)=>{
+    res.render("index")
+})
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Server Started On ${PORT} Port`);
+})
