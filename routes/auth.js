59bfa08dfe01377e4c291056fbe13691b80454a8
akbaralicoding <nematjonovakbarali19@gmail.com>
2024-02-01 22:02:19 +0500

    Autharation

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,26 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/login', (req, res)=>{
-    res.render("login")
+    res.render("login", {
+        title: 'Login | coderKing',
+        isLogin: true
+    })
 })
 router.get('/signup', (req, res)=>{
-    res.render("signup")
+    res.render("signup", {
+        title: 'Register | coderKing',
+        isSignup: true
+    })
 })
+router.post('/register', (req, res)=>{
+    console.log(req.body); 
+    res.redirect('/')
+})
+
+router.post('/login', (req, res)=>{
+    console.log(req.body); 
+    res.redirect('/')
+})
+
 
 export default router
--- routes/products.js
+++ routes/products.js
@@ -3,15 +3,23 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/', (req, res)=>{
-    res.render("index")
+    res.render("index", {
+        title: 'Boom Shop | coderKing'
+    })
 })
 
 
 router.get('/add', (req, res)=>{
-    res.render("add")
+    res.render("add", {
+        title: 'Add Product | coderKing',
+        isAdd: true
+    })
 })
 router.get('/products', (req, res)=>{
-    res.render("products")
+    res.render("products", {
+        title: 'Products | coderKing',
+        isProduct: true
+    })
 })
 
 export default router
