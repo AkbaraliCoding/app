8bd6dd364214e14e37d0126a985196b7c6be5196
akbaralicoding <nematjonovakbarali19@gmail.com>
2024-02-03 11:39:52 +0500

    mongo db connected

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 import express from 'express'
-import {create} from 'express-handlebars'
+import { create } from 'express-handlebars'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
+// routes
 import authRoutes from './routes/auth.js'
 import productsRoutes from './routes/products.js'
+dotenv.config()
 
 const app = express()
+const url = process.env.MONGO_URL || " " 
 
 const hbs = create({
     defaultLayout: "main",
-    extname:"hbs"
+    extname: "hbs"
 })
 
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
-app.use(express.urlencoded({extended: true}))
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(express.json())
 
 app.use(authRoutes)
 app.use(productsRoutes)
 
-const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
-    console.log(`Server Started On ${PORT} Port`);
-})
+const startApp = () => {
+	try {
+		mongoose.set('strictQuery', false)
+		mongoose.connect(url, {useNewUrlParser: true}, () => console.log('Mongo DB connected'))
+
+		const PORT = process.env.PORT || 3000
+		app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+startApp()
+//mongodb+srv://akbarali:<password>@cluster0.3wgy2t8.mongodb.net/?retryWrites=true&w=majority
